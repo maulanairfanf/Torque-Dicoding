fbb109cc0e2a3fdd7a06821aa165556a55e419d3
maulanairfanf <maulanairfanf@gmail.com>
2020-04-24 03:02:16 +0700

    tahap 1

---
+++ app.js
@@ -0,0 +1,32 @@
+function frontendmore() {
+    document.getElementById("frontend").innerHTML = "front-end dari sebuah website adalah bagian yang langsung dilihat oleh user.Bagian ini dibangun menggunakan HTML, CSS, dan JavaScript."
+}
+
+function frontendless() {
+    document.getElementById("frontend").innerHTML = ""
+}
+
+
+function backendmore() {
+    document.getElementById("backend").innerHTML = "Back-end adalah bagian belakang layar dari sebuah website. Bahasa pemograman untuk back-end development diantaranya adalah PHP, Ruby, Python, dan banyak lainnya."
+}
+
+function backendless() {
+    document.getElementById("backend").innerHTML = ""
+}
+
+function uiuxmore() {
+    document.getElementById("uiux").innerHTML = "User Interface dan User experience yakni merupakan sebuah tampilan visual dalam sebuah aplikasi atau alat pemasaran digital dalam bentuk website yang dapat meningkatkan brand yang dimiliki oleh bisnis atau perusahaan yang anda miliki."
+}
+
+function uiuxless() {
+    document.getElementById("uiux").innerHTML = ""
+}
+
+function change1() {
+    document.getElementById("picture").src = 'assets/kirito.jpg';
+}
+
+function change2() {
+    document.getElementById("picture").src = 'assets/maulana.jpg';
+}
